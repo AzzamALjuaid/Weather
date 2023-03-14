6975ef05ab9bf9641714820a17dac38af80bb136
AzzamALjuaid <92253516+AzzamALjuaid@users.noreply.github.com>
2023-03-15 00:44:37 +0300

    Second commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,7 @@ class MyHomePage extends StatelessWidget {
             Container(
               height: size.height * 0.75,
               width: size.width,
+              padding: EdgeInsets.only(top: 30),
               margin: EdgeInsets.only(right: 9, left: 9),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
@@ -29,19 +30,180 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   Text(
-                    'Constantine',
+                    'Taif',
+                    style: TextStyle(color:Colors.white.withOpacity(0.9),
+                        fontSize: 40,
+                        fontFamily: 'Kanit'),
+                  ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'monday 14 03 2023 ',
+                style: TextStyle(color:Colors.white.withOpacity(0.9),
+                    fontSize: 20,
+                    fontFamily: 'Lobster'),
+              ),
+               Image.asset(
+                   'assets/icons/sun.png',
+               width: size.width * 0.4,
+               ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Sunny',
                     style: TextStyle(color:Colors.white,
-                        fontSize: 35,
-                        fontFamily: 'Kanit' ),
-                  )
+                        fontSize: 40,
+                        fontWeight: FontWeight.w300 ,
+                        fontFamily: 'Lobster'),
+                  ),
+              SizedBox(
+                height: 5,
+                ),
+                  Text(
+                    '30°',
+                    style: TextStyle(color:Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500 ,
+                        fontFamily: 'Lobster'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                    Expanded(
+                        child: Column(children: [
+                      Image.asset('assets/icons/wind.png',
+                      width: size.width * 0.18
+                        ,),
+                      Text(
+                        '15.5 Km/h',
+                      style: TextStyle(
+                        color: Colors.white,
+                          fontFamily: 'Lobster',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        'Wind',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.7),
+                            fontFamily: 'Lobster',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                    )
+                    ),
+                      Expanded(child: Column(children: [
+                        Image.asset('assets/icons/clouds.png',
+                          width: size.width * 0.18
+                          ,),
+                        Text(
+                          '50',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Lobster',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Humidity',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontFamily: 'Lobster',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                      )
+                      ),
+                      Expanded(child: Column(children: [
+                        Image.asset('assets/icons/wind-flag.png',
+                          width: size.width * 0.18
+                          ,),
+                        Text(
+                          'NE',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Lobster',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Wind Direction',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontFamily: 'Lobster',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                      )
+                      ),
+                  ],
+                  ),
                 ],
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(child: Column(
+                  children: [
+                    Text(
+                      'Gust',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontFamily: 'Kanit',
+                        fontSize: 18),
+                    ),
+                    Text(
+                      '20.0 Km/h',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Kanit',
+                          fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Gust',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontFamily: 'Kanit',
+                          fontSize: 18),
+                    ),
+                    Text(
+                      '20.0 Km/h',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Kanit',
+                          fontSize: 20),
+                    ),
+
+                  ],
+                ),
+                ),
+              ],
+            ),
           ],
         ),
-
       ),
-
     );
   }
 }
